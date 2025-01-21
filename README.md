12916a60537dd739435512bd7122a8827518f677
Sakchai Sengsui <sakchai.sengsui@gmail.com>
2025-01-21 11:38:50 +0700

    init commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# looxtv-player-sdk
---
+++ looxtv-player-sdk/src/main/java/net/thaicom/sdk/looxtv/LooxPlayer.java
@@ -0,0 +1,13 @@
+package net.thaicom.sdk.looxtv;
+
+import android.util.Log;
+
+public class LooxPlayer {
+    private static final String TAG = "LooxPlayer";
+    public LooxPlayer(Object context) {
+    }
+
+    public static void PrintLog(String message) {
+        Log.d(TAG, message);
+    }
+}
